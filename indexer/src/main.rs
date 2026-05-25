5622c81782e4ce501b5fffe5348dd589b1a9db70
Rohit <rohitmandavkar3577@gmail.com>
2026-05-25 13:32:41 +0530

    add image and video processing.

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -6,7 +6,6 @@ mod media;
 use clap::Parser;
 use std::path::PathBuf;
 
-
 #[derive(Debug, Parser)]
 #[command(author, version, about = "Vault Indexer")]
 pub struct CliArgs{
@@ -22,7 +21,20 @@ fn main() {
 
     let notes = indexer::run(&args.vault);
     println!("parsed {} notes", notes.len());
+    
+    // collect all media: videos and images across all notes in short FETCH
+    let all_images: Vec<String> = notes.iter().flat_map(|n| n.images.clone()).collect();
+    let all_videos: Vec<String> = notes.iter().flat_map(|n| n.videos.clone()).collect();
 
+    // processing of media
+    if !all_images.is_empty(){
+        println!("processing {} images...", all_images.len());
+        media::process_images(&args.vault, &all_images).expect("Images Processing Failed");
+    }
+    if !all_videos.is_empty(){
+        println!("processing {}", all_videos.len());
+        media::process_videos(&args.vault, &all_videos).expect("Video Processing Failed")
+    }
     let conn = db::init(args.db.to_str().unwrap()).expect("failed to open db");
     db::write_all(&conn, &notes).expect("Failed to write to db");
 
